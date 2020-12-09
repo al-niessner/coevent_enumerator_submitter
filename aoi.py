90abf21d7d0037cd9e066f2a104e2a8b38e0377d
Al Niessner <Al.Niessner@xxx.xxx>
2020-12-09 11:34:34 -0800

    add next part(s)

---
+++ aoi.py
@@ -0,0 +1,19 @@
+'''module processes an actual AOI
+
+This is the bulk of the pseudo code in the README.md that is inside the
+outermost loop. It was separated from iterate.py simply to make is trivial
+to move it into its own job mechanism.
+'''
+
+def process (aoi):
+    '''process the AOI as described by the pseudo code in README.md
+    '''
+    return
+
+def update (aoi):
+    '''write the AOI back out to ES
+
+    Much of the AOI processing updates the the state information and it needs
+    to be recorded in ES.
+    '''
+    return
--- iterate.py
+++ iterate.py
@@ -1,36 +1,39 @@
 '''module that orchestrates all of the work
 '''
 
+import aoi
 import datetime
 import es
 import es.request
 
-def initialize (aoi):
+def initialize (data):
     '''add state information that this processing needs
 
     Check the AOI for the state information. If it does not exist, then add it.
     '''
-    if 'event_processing' not in aoi:
-        aoi['event_processing'] = {
+    if 'event_processing' not in data:
+        data['event_processing'] = {
             'post':{'acqs':[], 'count':0, 'slcs':[], 'threshold':3,
                     'time_blackout_in_seconds':86400},
             'pre':{'acqs':[], 'count':0, 'slcs':[], 'threshold':3,
                    'time_blackout_in_seconds':86400},
             'previous':'',
             }
-        td = datetime.timedelta(seconds=aoi['event_processing']['post']['time_blackout_in_seconds'])
-        et = datetime.datetime.fromisoformat(aoi['metadata']['eventtime'])
+        td = datetime.timedelta(seconds=data['event_processing']['post']['time_blackout_in_seconds'])
+        et = datetime.datetime.fromisoformat(data['metadata']['eventtime'])
         prev = et + td
-        aoi['event_processing']['previous'] = prev.isoformat('T','seconds')+'Z'
+        data['event_processing']['previous'] = prev.isoformat('T','seconds')+'Z'
+        aoi.update (data)
         pass
     return
 
 def main():
     '''the main processing block -- find and loop over all active AOIs'''
     for response in es.query (es.request.ALL_ACTIVE_AOI):
-        aoi = response['_source']
-        initialize (aoi)
-        print (aoi)
+        data = response['_source']
+        initialize (data)
+        print (data)
+        aoi.process (data)
         pass
     return
 
