2ffdb5baf5581ddcbf413d45b546b8e337374122
Al Niessner <Al.Niessner@xxx.xxx>
2020-12-09 17:00:32 -0800

    new support elements

---
+++ orbit.py
@@ -0,0 +1,5 @@
+
+def load (acquisition):
+    '''load orbit files of highest precision for given acquisition
+    '''
+    return
---
+++ slc.py
@@ -0,0 +1,7 @@
+
+def load (acquisition):
+    '''load SLC from DAACs if it is not already here
+
+    This is going to send jobs to a Localizer queue.
+    '''
+    return
