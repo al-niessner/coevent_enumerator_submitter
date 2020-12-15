42157642e5d8dba4d12f545625dbf36cd1ddcc9a
Al Niessner <Al.Niessner@xxx.xxx>
2020-12-15 12:47:45 -0800

    start handing the geo intersection

--- footprint.py
+++ footprint.py
@@ -1,5 +1,17 @@
 '''transact with geo-data and satelite data'''
 
+import json
+import osgeo.ogr
+
+def convert (acq, eof=None):
+    '''convert an object with ['location'] to a shapely polygon'''
+    poly = osgeo.ogr.CreateGeometryFromJson(json.dumps(acq['location']))
+
+    # if an orbit file is given, the turn into footprint
+    if eof:
+        pass
+    return poly
+
 def coverage (aoi, acqs, eofs):
     '''compute the percentage of the coverage
 
@@ -8,5 +20,15 @@ def coverage (aoi, acqs, eofs):
 
     The result is the area(intersection)/area(aoi['location'])*100
     '''
-    percent = 0.0
+    fps = [convert (acq, eof) for acq,eof in zip(acqs,eofs)]
+    whole_fp = union (fps)
+    aoi_ = convert (aoi)
+    intersection = aoi_.intersection (whole_fp)
+    percent = intersection.area() / aoi_.area() * 100.
     return percent
+
+def union (polys):
+    '''Create the union of a list of shapely polygons'''
+    result = polys[0]
+    for poly in polys[1:]: result = result.Union (poly)
+    return result
