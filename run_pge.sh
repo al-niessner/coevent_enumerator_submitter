de0be7a89a5559d3f83d9211db1c50e8d97346aa
Al Niessner <Al.Niessner@xxx.xxx>
2020-12-16 13:22:16 -0800

    make run script simpler

--- run_pge.sh
+++ run_pge.sh
@@ -1,18 +1,9 @@
 #! /usr/bin/env bash
 
 # source ISCE env
-export GMT_HOME=/usr/local/gmt
-export ARIAMH_HOME=$HOME/ariamh
-export STANDARD_PRODUCT_HOME=$HOME/standard_product
-source $ARIAMH_HOME/isce.sh
-source $ARIAMH_HOME/giant.sh
+source /opt/isce2/isce_env.sh
 export TROPMAP_HOME=$HOME/tropmap
-export UTILS_HOME=$ARIAMH_HOME/utils
-export GIANT_HOME=/usr/local/giant/GIAnT
-export PYTHONPATH=${HOME}/verdi/etc:$ISCE_HOME/applications:$ISCE_HOME/components:$BASE_PATH:$ARIAMH_HOME:$TROPMAP_HOME:$GIANT_HOME:$PYTHONPATH
-export PATH=$BASE_PATH:$TROPMAP_HOME:$GMT_HOME/bin:$PATH
 
 # source environment
 source $HOME/verdi/bin/activate
-
-python /home/ops/verdi/ops/coevent_enumerator_submitter/iterate,py
+/home/ops/verdi/ops/coevent_enumerator_submitter/iterate.py
