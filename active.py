a01cd58c838bfa464971367cae487e702753f8f9
Al Niessner <Al.Niessner@xxx.xxx>
2020-12-23 16:39:21 -0800

    purge data before writing

--- active.py
+++ active.py
@@ -9,8 +9,6 @@ import collections
 import datetime
 import es.request
 import footprint
-import hysds.dataset_ingest
-import hysds.utils
 import json
 import orbit
 import os
@@ -134,8 +132,9 @@ def update (aoi):
     Much of the AOI processing updates the the state information and it needs
     to be recorded in ES.
     '''
-    # FIXME: need to update AOI in ES
+    # FIXME: need to update AOI in ES not create a zesty-test-data
     label = 'zesty-test-data-' + aoi['id']
+    es.purge (label, aoi['version'])
 
     if not os.path.exists (label): os.makedirs (label, 0o755)
 
