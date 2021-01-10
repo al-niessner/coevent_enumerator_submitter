6eaeb0289bc1875ffcd61fc2d7458e52ab84ccf8
Al Niessner <Al.Niessner@xxx.xxx>
2021-01-10 10:59:22 -0800

    rearrange the start up to reflect the build

--- run_pge.sh
+++ run_pge.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
 
-# source ISCE env
-source /opt/isce2/isce_env.sh
-export TROPMAP_HOME=$HOME/tropmap
-
 # source environment
 source $HOME/verdi/bin/activate
+
+# source ISCE env
+source /opt/isce2/isce_env.sh
+
+# do the actual PGE work
 /home/ops/verdi/ops/coseismic_enumerator/iterate.py
 
 # the sleep is to keep the container around for debugging purposes
