8e75f048600f44b8d814b2ce18391177bc02736b
Al Niessner <Al.Niessner@xxx.xxx>
2021-03-05 11:12:10 -0800

    define parameters

--- submit.py
+++ submit.py
@@ -5,7 +5,24 @@ import json
 import requests
 import sys
 
-job_params = {}
+job_params = [{"destination": "context",
+               "name": "coverage_threshold_percent",
+               "value": 90},
+              {"destination": "context",
+               "name": "post_count",
+               "value": 3},
+              {"destination": "context",
+               "name": "post_buffer_in_seconds",
+               "value": 60},
+              {"destination": "context",
+               "name": "prior_count",
+               "value": 3},
+              {"destination": "context",
+               "name": "prior_buffer_in_seconds",
+               "value": 60},
+              {"destination": "context",
+               "name": "reset_all",
+               "value": 0}]
 job_release = 'step_11'
 job_type = 'job-enumerator'
 queue = 'factotum-job_worker-coseismic-enumerator'
@@ -16,7 +33,7 @@ params = {
     'job_name': job_type,
     'tags': tag_name,
     'type': "{}:{}".format(job_type, job_release),
-    'params': json.dumps(job_params),
+    'params': job_params,
     'enable_dedup': False
 }
 req = requests.post(sys.argv[1], params=params, verify=False)
